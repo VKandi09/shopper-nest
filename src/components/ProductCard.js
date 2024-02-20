bf9e85e85d66fd3c240fc1867bcfa1f8f9a40e29
Varsha Kandi <varsha.kandi@ellucian.com>
2024-02-20 00:36:10 -0500

    Added new changes to the shopping cart using Context and Reducers

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useCart } from '../context/CartContext';
 import './ProductCard.css';
 
 export const ProductCard = ({product}) => {
-  const { image, name, price } = product;
+  const { cartList, addToCart, removeFromCart } = useCart();
+  const { id, image, name, price } = product;
+  const [ isInCart, setIsInCart ] = useState(false);
+
+  useEffect(() => {
+    const productIsInCart = cartList.find(cartItem => cartItem.id === id)
+    if (productIsInCart) {
+      setIsInCart(true);
+    } else {
+      setIsInCart(false);
+    }
+  }, [cartList, id])
+
   return (
       <div className='productCard'>
         <img src={image} alt={name} />
         <p className='name'>{name}</p>
         <div className='action'>
           <p>${price}</p>
-          <button>Add to Cart</button>
+          { isInCart ? <button className='remove' onClick={() => removeFromCart(product)}>Remove</button>: <button onClick={() => addToCart(product)}>Add to Cart</button>}
+          
         </div>
       </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import './index.css';
 import App from './App';
 
@@ -8,6 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <CartProvider>
+        <App />
+      </CartProvider>
       <App />
     </Router>
   </React.StrictMode>
